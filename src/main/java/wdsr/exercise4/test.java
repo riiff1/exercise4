7e3877fb615431d2350e6fcf89e034a9bd77f6d3
riiff1 <riiff@wp.pl>
2017-04-23 23:55:34 +0200

    test2 commit on new branch

--- src/main/java/wdsr/exercise4/test.java
+++ src/main/java/wdsr/exercise4/test.java
@@ -6,7 +6,6 @@ package wdsr.exercise4;
 public class test {
     public static void main(String[] args) {
         System.out.println("test");
-        String s = "test";
-        System.out.println("test");
+        System.out.println("test3");
     }
 }
